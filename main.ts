912e10988b64dbfc8942a4379020138a78f72c76
lilcryptotech <lilcryptotech@gmail.com>
2024-04-13 08:25:12 -0400

    switched to ts

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-console.log("test");
+import { ip, ipv6, mac } from 'address';
+var prompt = require('prompt-sync')();
+
+
+while (true) {
+var choice = prompt('Please choose an option\n');
+
+if (choice === "1") {
+    console.log(`ip address is ${ip()}`);
+    break;
+} 
+
+
+
+
+
+
+else{
+console.log('invalid choice')
+
+}
+};
