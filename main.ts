e92a8e60df4a311519c92d3f58fe19e38f6f82a4
lilcryptotech <lilcryptotech@gmail.com>
2024-04-13 11:20:10 -0400

    added try/catch and async function

--- main.ts
+++ main.ts
@@ -1,20 +1,33 @@
-import { ip, ipv6, mac } from 'address';
-var prompt = require('prompt-sync')();
+import { ip, ipv6 } from 'address';
+const prompt = require('prompt-sync')();
 
-
-while (true) {
-var choice = prompt('Please choose an option\n');
-
-if (choice === "1") {
-    console.log(`ip address is ${ip()}`);
-    
-    break;
-} 
-else if (choice==="2"){
-console.log(`the ipv6 address is ${ipv6()}`);
-break;
-}else{
-console.log('invalid choice')
-
+function sleep(seconds: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
 }
-};
+
+async function main() {
+    while (true) {
+        try {
+            const choice = prompt('Please choose an option\n');
+
+            if (choice === "1") {
+                console.log(`ip address is ${ip()}`);
+                await sleep(5);
+                break; 
+            } 
+            else if (choice === "2") {
+                console.log(`the ipv6 address is ${ipv6()}`);
+                await sleep(5);
+                break;
+            } 
+            else {
+                console.log('invalid choice');
+            }
+        } catch (err) {
+            console.error('An error occurred:');
+        }
+    }
+}
+
+
+main();
